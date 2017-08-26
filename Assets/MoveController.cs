af694415e0b5e37ebdc509e59cceea6485931bcd
王冲 <654306663@qq.com>
2017-08-26 16:58:50 +0800

    添加相机左右旋转、前后缩放、跟随玩家功能

---
+++ Assets/MoveController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+    Transform camera;
+
+    Animator ani;
+
+    NavMeshAgent nav;
+
+    float h, v;
+
+    Vector3 moveVec;
+
+    // Use this for initialization
+    void Start () {
+
+        camera = Camera.main.transform;
+        ani = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        moveVec = new Vector3(h, 0, v);
+
+        if (h != 0 || v != 0)
+        {
+            ani.SetBool("Run", true);
+            // 根据摄像机方向 进行移动
+            moveVec = Quaternion.Euler(0, camera.eulerAngles.y, 0) * moveVec;
+            nav.Move(moveVec * Time.deltaTime * 5);
+            RotatePlayer();
+        }
+        else
+        {
+            ani.SetBool("Run", false);
+        }
+	}
+
+    private void RotatePlayer()
+    {
+        //向量v围绕y轴旋转cameraAngle.y度
+        Vector3 vec = Quaternion.Euler(0, 0, 0) * moveVec;
+        Quaternion qua = Quaternion.LookRotation(vec);
+        transform.rotation = Quaternion.Lerp(transform.rotation, qua, Time.deltaTime * 100);
+    }
+}
